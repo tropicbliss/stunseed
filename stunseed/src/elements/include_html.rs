ea9c75be692f467136959ecc19c41aa744a232bc
tropicbliss <tropicbliss@protonmail.com>
2024-08-30 16:02:01 +0800

    Use normal comment syntax

--- stunseed/src/elements/include_html.rs
+++ stunseed/src/elements/include_html.rs
@@ -2,6 +2,13 @@
 macro_rules! include_html {
     ($path:expr) => {{
         let source = include_str!($path);
-        crate::elements::text(source)
+        embed_html!(source)
+    }};
+}
+
+#[macro_export]
+macro_rules! embed_html {
+    ($source:expr) => {{
+        crate::elements::text::text($source)
     }};
 }
