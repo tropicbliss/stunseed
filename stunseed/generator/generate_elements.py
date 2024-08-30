c0acf18b433cbe1860bd35a691807209f5ca0037
tropicbliss <tropicbliss@protonmail.com>
2024-08-30 12:57:48 +0800

    use derive macro

---
+++ stunseed/generator/generate_elements.py
@@ -0,0 +1,109 @@
+import json
+import os
+
+def replace_strings(s: str):
+    return s.replace("\"", "\\\"")
+
+def build_int_attribute(function_name, doc, canonical):
+    doc = replace_strings(doc)
+    return f"""
+#[doc = "{doc}"]
+    #[inline]
+    pub fn {function_name}(mut self: Box<Self>, value: i64) -> Box<Self> {{
+        self.inner.insert_attribute(
+            "{canonical}",
+            crate::ast::AttributeValue::KeyValuePair(value.to_string().into()),
+        );
+        self
+    }}
+"""
+
+def build_string_attribute(function_name, doc, canonical):
+    doc = replace_strings(doc)
+    return f"""
+#[doc = "{doc}"]
+    #[inline]
+    pub fn {function_name}<T>(mut self: Box<Self>, value: T) -> Box<Self>
+    where
+        T: Into<::std::borrow::Cow<'static, str>>,
+    {{
+        self.inner.insert_attribute(
+            "{canonical}",
+            crate::ast::AttributeValue::KeyValuePair(value.into()),
+        );
+        self
+    }}
+"""
+
+def build_float_attribute(function_name, doc, canonical):
+    doc = replace_strings(doc)
+    return f"""
+#[doc = "{doc}"]
+    #[inline]
+    pub fn {function_name}(mut self: Box<Self>, value: f64) -> Box<Self> {{
+        self.inner.insert_attribute(
+            "{canonical}",
+            crate::ast::AttributeValue::KeyValuePair(value.to_string().into()),
+        );
+        self
+    }}
+"""
+
+def build_bool_attribute(function_name, doc, canonical):
+    doc = replace_strings(doc)
+    return f"""
+#[doc = "{doc}"]
+    #[inline]
+    pub fn {function_name}(mut self: Box<Self>, value: bool) -> Box<Self> {{
+        self.inner.insert_attribute(
+            "{canonical}",
+            crate::ast::AttributeValue::BooleanAttribute(value),
+        );
+        self
+    }}
+"""
+
+def build(struct_name, element_name, is_void, contains_global, url_link):
+    result = ""
+    result += f"""
+#[derive(Clone, HtmlElement)]
+pub struct {struct_name} {{
+    inner: crate::ast::DomElement,
+}}
+
+#[doc = "The HTML `<{element_name}>` element\\n\\n[MDN Documentation]({url_link})"]
+pub fn {element_name}() -> Box<{struct_name}> {{
+    Box::new({struct_name} {{
+        inner: crate::ast::DomElement::{"new_void" if is_void else "new"}("{element_name}"),
+    }})
+}}\n
+"""
+    if not is_void and not element_name == "html":
+        result += f"unsafe impl crate::html::NonVoidHtmlElement for {struct_name} {{}}"
+    if contains_global:
+        result += f"impl crate::attributes::ContainsGlobalAttributes for {struct_name} {{}}\n"
+    return result
+
+glo_elem_res = ""
+
+directory_path = "data/merged/elements"
+for filename in os.listdir(directory_path):
+    file_path = os.path.join(directory_path, filename)
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+        glo_elem_res += build(data["struct_name"], data["tag_name"], not data["has_closing_tag"], data["has_global_attributes"], data["mdn_link"])
+        glo_elem_res += f"\nimpl {data["struct_name"]} {{"
+        for attribute in data["attributes"]:
+            match attribute["ty"]:
+                case "String":
+                    glo_elem_res += build_string_attribute(attribute["field_name"], attribute["description"], attribute["name"])
+                case "Bool":
+                    glo_elem_res += build_bool_attribute(attribute["field_name"], attribute["description"], attribute["name"])
+                case "Integer":
+                    glo_elem_res += build_int_attribute(attribute["field_name"], attribute["description"], attribute["name"])
+                case "Float":
+                    glo_elem_res += build_float_attribute(attribute["field_name"], attribute["description"], attribute["name"])
+        glo_elem_res += "}\n"
+
+with open("../src/generated/gen_elements.rs", 'w') as file:
+    file.write(glo_elem_res)
---
+++ stunseed/generator/generate_global_attributes.py
@@ -0,0 +1,83 @@
+import json
+
+def replace_strings(s: str):
+    return s.replace("\"", "\\\"")
+
+def build_int_attribute(function_name, doc, canonical):
+    doc = replace_strings(doc)
+    return f"""
+#[doc = "{doc}"]
+    #[inline]
+    fn {function_name}(mut self: Box<Self>, value: i64) -> Box<Self> {{
+        self.get_dom_element_mut().insert_attribute(
+            "{canonical}",
+            crate::ast::AttributeValue::KeyValuePair(value.to_string().into()),
+        );
+        self
+    }}
+"""
+
+def build_float_attribute(function_name, doc, canonical):
+    doc = replace_strings(doc)
+    return f"""
+#[doc = "{doc}"]
+    #[inline]
+    fn {function_name}(mut self: Box<Self>, value: f64) -> Box<Self> {{
+        self.get_dom_element_mut().insert_attribute(
+            "{canonical}",
+            crate::ast::AttributeValue::KeyValuePair(value.to_string().into()),
+        );
+        self
+    }}
+"""
+
+def build_string_attribute(function_name, doc, canonical):
+    doc = replace_strings(doc)
+    return f"""
+#[doc = "{doc}"]
+    #[inline]
+    fn {function_name}<T>(mut self: Box<Self>, value: T) -> Box<Self>
+    where
+        T: Into<::std::borrow::Cow<'static, str>>,
+    {{
+        self.get_dom_element_mut().insert_attribute(
+            "{canonical}",
+            crate::ast::AttributeValue::KeyValuePair(value.into()),
+        );
+        self
+    }}
+"""
+
+def build_bool_attribute(function_name, doc, canonical):
+    doc = replace_strings(doc)
+    return f"""
+#[doc = "{doc}"]
+    #[inline]
+    fn {function_name}(mut self: Box<Self>, value: bool) -> Box<Self> {{
+        self.get_dom_element_mut().insert_attribute(
+            "{canonical}",
+            crate::ast::AttributeValue::BooleanAttribute(value),
+        );
+        self
+    }}
+"""
+
+glo_attr_res = "pub trait ContainsGlobalAttributes: crate::html::HtmlElement {"
+
+with open("data/manual/global_attributes.json", 'r') as file:
+    attributes = json.load(file)
+    for attribute in attributes:
+        match attribute["ty"]:
+            case "String":
+                glo_attr_res += build_string_attribute(attribute["field_name"], attribute["description"], attribute["name"])
+            case "Bool":
+                glo_attr_res += build_bool_attribute(attribute["field_name"], attribute["description"], attribute["name"])
+            case "Integer":
+                glo_attr_res += build_int_attribute(attribute["field_name"], attribute["description"], attribute["name"])
+            case "Float":
+                glo_attr_res += build_float_attribute(attribute["field_name"], attribute["description"], attribute["name"])
+
+glo_attr_res += "}"
+
+with open("../src/generated/gen_global_attributes.rs", 'w') as file:
+    file.write(glo_attr_res)
